ea944810198baad5ee8afdf0d18cfa22aa0d39a2
Alfiya-1336 <salfiya695@gmail.com>
2026-07-12 16:41:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-const API_KEY = rsk_01KXAX2Y33Z2EV173JN3SAJHTS;
+async function solve() {
+
+    const question = document.getElementById("question").value;
+    const answer = document.getElementById("answer");
+
+    answer.innerHTML = "Thinking...";
+
+    try {
+
+        const response = await fetch("https://api.meshapi.ai/v1/chat/completions", {
+
+            method: "POST",
+
+            headers: {
+
+                "Content-Type": "application/json",
+
+                "Authorization": "Bearer rsk_01KXAX2Y33Z2EV173JN3SAJHTS"
+
+            },
+
+            body: JSON.stringify({
+
+                model: "openai/gpt-4o-mini",
+
+                messages: [
+
+                    {
+
+                        role: "user",
+
+                        content: question
+
+                    }
+
+                ]
+
+            })
+
+        });
+
+        const data = await response.json();
+
+        answer.innerHTML = data.choices[0].message.content;
+
+    }
+
+    catch(error){
+
+        answer.innerHTML = "Something went wrong.";
+
+        console.log(error);
+
+    }
+
+}
